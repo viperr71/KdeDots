a5e0b596c8560f23a156e5404bcabbd370a5c3dc
Bamavic <bamavic1971@gmail.com>
2021-01-02 21:23:36 -0500

    fix polybar not launching on all monitors at startup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload xmonad &
-done
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload xmonad &
+  done
+else
+  polybar --reload xmonad &
+fi
 
 echo "Bars launched..."
